ef9099a05d5a3de4e5fc709c868d6062269e9107
JochemTUe <63041722+JochemV01@users.noreply.github.com>
2023-01-24 12:50:57 +0100

    sounds added to stupid buttons

--- Assets/tasks_script.cs
+++ Assets/tasks_script.cs
@@ -2,25 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Threading;
+
 
 
 public class tasks_script : MonoBehaviour
 {
-public void AcceptTask(){
-        //Destroy(thing, 1);
-        SceneManager.LoadScene("minigame_holder_scene");
-    }
+        public StartSound mySoundScript;
 
-public void WinGameButton(){
-        SceneManager.LoadScene("ProjectFinished");
-}
+        void Start() 
+        {
+                mySoundScript = GameObject.FindObjectOfType(typeof(StartSound)) as StartSound;
+        }
 
-public void CloseWindow(){
-        SceneManager.LoadScene("SampleScene");
-}
+        public void AcceptTask(){
+                //Destroy(thing, 1);
+                mySoundScript.PlayButton();
+                // Thread.Sleep(1000);
+                SceneManager.LoadScene("minigame_holder_scene");
+        }
 
-public void DeclineProject(GameObject thing){
-        Destroy(thing, 1);
+        public void WinGameButton(){
+                mySoundScript.PlayButton();
+                // Thread.Sleep(1000);
+                SceneManager.LoadScene("ProjectFinished");
+        }
+
+        public void CloseWindow(){
+                mySoundScript.PlayButton();
+                // Thread.Sleep(1000);
+                SceneManager.LoadScene("SampleScene");
+        }
+
+        public void DeclineProject(GameObject thing){
+                // Thread.Sleep(100);
+                mySoundScript.PlayButton();
+                Destroy(thing, 1);
 }
 
 void Update()
