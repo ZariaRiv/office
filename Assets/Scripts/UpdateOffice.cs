464343d8808e1e1ad56f8ac00bb26bcdaac35acf
lisarabbers <l.h.rabbers@student.tue.nl>
2023-01-25 00:35:53 +0100

    fixed office updates & colors

--- Assets/Scripts/UpdateOffice.cs
+++ Assets/Scripts/UpdateOffice.cs
@@ -11,24 +11,40 @@ public class UpdateOffice : MonoBehaviour
     public ProgressVisualiserPrototype progress;
 
     // level 1
-    public GameObject couchObject;
-    
+    public GameObject chairObject;
+    List<GameObject> chairList = new();
+
     // level 2
-    public GameObject snakePlantObject;
+    public GameObject couchObject;
 
     // level 3
-    public GameObject bookObject;
+    public GameObject plantObject;
+    public GameObject endTableObject;
 
     // level 4
-
+    public GameObject tableObject;
+    
     // level 5
+    public GameObject snakePlantObject;
 
 
     // Start is called before the first frame update
     void Start()
     {
         minigameCommunicator.woodWon += () => UpdateOffices(TR);
-        Debug.Log("couch is placed");
+
+        // GameObject couch = Instantiate(couchObject, new Vector3(-3, 0.56f, 40), transform.rotation);
+        // couch.transform.Rotate(0, 45, 0);       
+        
+        // GameObject plant = Instantiate(snakePlantObject, new Vector3(-18.5f, 0f, 40), transform.rotation);
+
+
+    }
+
+    private GameObject CreateChair(float x){
+        GameObject chair = Instantiate(chairObject, new Vector3(x, 0.5923386f, 37.35f), transform.rotation);
+        chair.transform.Rotate(0, 180, 0);
+        return chair;
     }
 
     // Update is called once per frame
@@ -107,23 +123,33 @@ public class UpdateOffice : MonoBehaviour
                 switch(levelTR){
                     case 1:
                         //add stuff
-                        GameObject couch = Instantiate(couchObject, new Vector3(0, 0, 0), transform.rotation);
-                        couch.transform.Rotate(0, 45, 0);
+                        for (int i = 0; i < 6; i++){
+                        chairList.Add(CreateChair(-18.13f + i*1.7f));
+                        }
                         break;
 
                     case 2:
                         //more stuff
-                        GameObject plant = Instantiate(snakePlantObject, new Vector3(-18.5f, 0f, 40), transform.rotation);
+                        GameObject couch = Instantiate(couchObject, new Vector3(-3, 0.56f, 40), transform.rotation);
+                        couch.transform.Rotate(0, 90, 0);
                         break;
 
                     case 3:
                         //even more stuff
+                        GameObject plant = Instantiate(plantObject, new Vector3(-6.13f, 1.17f, 40.58f), transform.rotation);       
+                        GameObject endTable = Instantiate(endTableObject, new Vector3(-6.13f, 0.6f, 40.58f), transform.rotation);  
                         break;
+
                     case 4:
                         // giant amounts of stuff
+                             
+                        GameObject table = Instantiate(tableObject, new Vector3(-4.7f, 0.2799848f, 39.85f), transform.rotation);
+                        table.transform.Rotate(0, 90, 0);               
                         break;
+                    
                     case 5:
                         // almost too much stuff
+                        GameObject snakeplant = Instantiate(snakePlantObject, new Vector3(-18.5f, 0f, 40), transform.rotation);
                         break;
                 }
             break;
